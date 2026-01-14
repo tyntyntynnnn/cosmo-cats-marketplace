f96fb02e6b938ffeadeb68990504bccc64e8e080
tyntyntynnnn <egor12748@gmail.com>
2026-01-14 21:37:00 +0200

    Add unit test for CosmoCatService

---
+++ src/test/java/com/cosmocats/marketplace/cat/CosmoCatServiceTest.java
@@ -0,0 +1,25 @@
+package com.cosmocats.marketplace.cat;
+
+import com.cosmocats.marketplace.feature.FeatureNotAvailableException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "feature.flags.cosmoCats=true"
+})
+class CosmoCatServiceTest {
+
+    @Autowired
+    private CosmoCatService cosmoCatService;
+
+    @Test
+    void shouldReturnCatsWhenFeatureEnabled() {
+        var cats = cosmoCatService.getCosmoCats();
+        assertEquals(3, cats.size());
+    }
+}
