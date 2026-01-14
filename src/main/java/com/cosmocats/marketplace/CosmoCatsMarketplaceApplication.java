1f3426dbea5ccb70d277439b6cfae35515d91b72
tyntyntynnnn <egor12748@gmail.com>
2026-01-14 21:17:09 +0200

    Add Spring Boot application entry point

---
+++ src/main/java/com/cosmocats/marketplace/CosmoCatsMarketplaceApplication.java
@@ -0,0 +1,12 @@
+package com.cosmocats.marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmoCatsMarketplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmoCatsMarketplaceApplication.class, args);
+    }
+}
