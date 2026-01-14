c03ce6dabca501d9739b09e604c0698d92f3089f
tyntyntynnnn <egor12748@gmail.com>
2026-01-14 21:37:29 +0200

    Add tests for feature toggle behavior

---
+++ src/test/java/com/cosmocats/marketplace/cat/CosmoCatServiceDisabledTest.java
@@ -0,0 +1,27 @@
+package com.cosmocats.marketplace.cat;
+
+import com.cosmocats.marketplace.feature.FeatureNotAvailableException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "feature.flags.cosmoCats=false"
+})
+class CosmoCatServiceDisabledTest {
+
+    @Autowired
+    private CosmoCatService cosmoCatService;
+
+    @Test
+    void shouldThrowExceptionWhenFeatureDisabled() {
+        assertThrows(
+                FeatureNotAvailableException.class,
+                () -> cosmoCatService.getCosmoCats()
+        );
+    }
+}
